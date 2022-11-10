669d6fc16c42d96a7d5753d552362cf9a6b5ee12
gothdawid <dawidstu@gmail.com>
2022-11-10 22:35:00 +0100

    init

---
+++ backend/prisma/migrations/20221110212113_added_timestamps/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Grade` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Group` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Message` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Schedule` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `grade` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `group` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `message` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `schedule` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;

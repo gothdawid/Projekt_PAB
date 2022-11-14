cf7efcb78a571e9b77cadc55817202a23991bf0c
Gianna <wojtasinska.asia@gmail.com>
2022-11-14 17:16:41 +0100

    Frontend: Header, Footer
    Created header and footer component
    Modified Header component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GroupListComponent } from './components/groups/group-list/group-list.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GroupListComponent
+    GroupListComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

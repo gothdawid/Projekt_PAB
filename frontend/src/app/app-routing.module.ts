26f0180b804deb1e7ff1993af2ced20ff71267eb
Gianna <wojtasinska.asia@gmail.com>
2022-11-23 18:02:52 +0100

    FRONTEND: Sign-in
    Add component signin
    Change color in footer
    Add hide header when signin page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent }
+  { path: "", component: HomeComponent },
+  { path: "sign-in", component: SigninComponent}
 ];
 
 @NgModule({

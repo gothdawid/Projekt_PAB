df00e32c1a693d10de5779cb3f6b6448fccd1236
asia <wojtasinska.asia@gmail.com>
2023-01-25 15:24:03 +0100

    FRONTEND: Account
    Added example view for account
    Change font in header

--- frontend/src/app/components/account/account.component.ts
+++ frontend/src/app/components/account/account.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent implements OnInit {
+  public account: any = {
+    id: 21,
+    firstName: 'Adam',
+    lastName: 'Spadam',
+    email: 'testowy@test.pl',
+    avatar: 'http://i.stack.imgur.com/Dj7eP.jpg',
+    group: '32',
+    address: {
+      id: 1,
+      street: 'Szkolna 2',
+      city: 'Zielona Góra'
+    }
+  }
 
   constructor() { }
 
